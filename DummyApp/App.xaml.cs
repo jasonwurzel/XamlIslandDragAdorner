82f351899d1d6c079513b686387315d566494313
Michael Kreich <michaelkreich@hotmail.com>
2019-09-17 16:03:46 +0200

    Add project files.

---
+++ DummyApp/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DummyApp
+{
+    sealed partial class App : Microsoft.Toolkit.Win32.UI.XamlHost.XamlApplication
+    {
+        public App()
+        {
+            this.Initialize();
+        }
+    }
+}
